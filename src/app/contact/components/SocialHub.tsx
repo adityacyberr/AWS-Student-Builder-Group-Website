b6cc4af4929a5982e17d4b9cd892a60efe15bfff
Adiii <1millionyouthoob@gmail.com>
2026-06-15 22:52:18 +0530

    fix: remove youtube, github and x from contact page social hub

--- src/app/contact/components/SocialHub.tsx
+++ src/app/contact/components/SocialHub.tsx
@@ -46,51 +46,6 @@ const SOCIAL_ITEMS: SocialItem[] = [
     hoverText: "group-hover:text-[#e1306c]",
   },
   {
-    id: "github",
-    name: "GitHub",
-    description: "Explore our projects & code",
-    href: "https://github.com/adityacyberr/AWS-Student-Builder-Group-Website",
-    icon: (
-      <svg className="h-6 w-6 fill-current" viewBox="0 0 24 24">
-        <path fillRule="evenodd" clipRule="evenodd" d="M12 2C6.477 2 2 6.477 2 12c0 4.42 2.87 8.17 6.84 9.5.5.08.66-.23.66-.5v-1.69c-2.77.6-3.36-1.34-3.36-1.34-.46-1.16-1.11-1.47-1.11-1.47-.9-.62.07-.6.07-.6 1 .07 1.53 1.03 1.53 1.03.9 1.52 2.34 1.07 2.91.83.09-.65.35-1.09.63-1.34-2.22-.25-4.55-1.11-4.55-4.92 0-1.11.38-2 1.03-2.71-.1-.25-.45-1.29.1-2.64 0 0 .84-.27 2.75 1.02.79-.22 1.65-.33 2.5-.33.85 0 1.71.11 2.5.33 1.91-1.29 2.75-1.02 2.75-1.02.55 1.35.2 2.39.1 2.64.65.71 1.03 1.6 1.03 2.71 0 3.82-2.34 4.66-4.57 4.91.36.31.69.92.69 1.85V21c0 .27.16.59.67.5C19.14 20.16 22 16.42 22 12A10 10 0 0012 2z" />
-      </svg>
-    ),
-    colorClass: "text-[#fafafa] bg-[#fafafa]/10 border-[#fafafa]/20",
-    hoverBorder: "hover:border-[#fafafa]/40",
-    hoverBg: "group-hover:bg-[#fafafa]/20",
-    hoverText: "group-hover:text-[#fafafa]",
-  },
-  {
-    id: "x",
-    name: "X (Twitter)",
-    description: "Latest announcements & insights",
-    href: "https://x.com/awsrimt",
-    icon: (
-      <svg className="h-5 w-5 fill-current" viewBox="0 0 24 24">
-        <path d="M18.244 2.25h3.308l-7.227 8.26 8.502 11.24H16.17l-5.214-6.817L4.99 21.75H1.68l7.73-8.835L1.254 2.25H8.08l4.713 6.231zm-1.161 17.52h1.833L7.084 4.126H5.117z" />
-      </svg>
-    ),
-    colorClass: "text-[#ffffff] bg-[#ffffff]/10 border-[#ffffff]/20",
-    hoverBorder: "hover:border-[#ffffff]/45",
-    hoverBg: "group-hover:bg-[#ffffff]/15",
-    hoverText: "group-hover:text-[#ffffff]",
-  },
-  {
-    id: "youtube",
-    name: "YouTube",
-    description: "Watch our talks & sessions",
-    href: "#",
-    icon: (
-      <svg className="h-6 w-6 fill-current" viewBox="0 0 24 24">
-        <path d="M23.498 6.163a3.003 3.003 0 0 0-2.11-2.11C19.517 3.545 12 3.545 12 3.545s-7.517 0-9.388.508a3.003 3.003 0 0 0-2.11 2.11C0 8.033 0 12 0 12s0 3.967.502 5.837a3.003 3.003 0 0 0 2.11 2.11c1.871.508 9.388.508 9.388.508s7.517 0 9.388-.508a3.003 3.003 0 0 0 2.11-2.11C24 15.967 24 12 24 12s0-3.967-.502-5.837zM9.545 15.568V8.432L15.818 12l-6.273 3.568z" />
-      </svg>
-    ),
-    colorClass: "text-[#ff0000] bg-[#ff0000]/10 border-[#ff0000]/20",
-    hoverBorder: "hover:border-[#ff0000]/40",
-    hoverBg: "group-hover:bg-[#ff0000]/20",
-    hoverText: "group-hover:text-[#ff0000]",
-  },
-  {
     id: "email",
     name: "Email",
     description: "Reach us directly anytime",
